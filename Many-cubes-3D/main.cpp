ae0a46f49a88932d1b066bdbb53edaebeada5cf0
Budzio <145849460+BudzioT@users.noreply.github.com>
2023-10-14 19:58:27 +0200

    Add files via upload

---
+++ Many-cubes-3D/main.cpp
@@ -0,0 +1,246 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "stb_image.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+#include <iostream>
+
+#include "Shader.h"
+
+void fixFrameBufferResize(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+
+const int winWidth = 800;
+const int winHeight = 600;
+
+
+int main()
+{
+	/********************************** Initialization ***********************************/
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(winWidth, winHeight, "Colorful triangle", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "ERROR::GLFW::Initialization_failed" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "ERROR::GLAD::Initialization_failed" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glViewport(0, 0, winWidth, winHeight);
+	glfwSetFramebufferSizeCallback(window, fixFrameBufferResize);
+
+	Shader shader("shaders/shader.vs", "shaders/shader.fs");
+
+	/********************************** Buffers ***********************************/
+	/* Vertices of the cube */
+	float vertices[] = {
+		/*		Positions	Texture coords*/
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f,  1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f,  1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f,  1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f,  1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f
+	};
+
+	/* Positions of the cubes in the world space */
+	glm::vec3 cubePositions[] = {
+		glm::vec3( 0.0f,   0.0f,   0.0f),
+		glm::vec3( 2.0f,   5.0f, -15.0f),
+		glm::vec3(-1.5f,  -2.2f,  -2.5f),
+		glm::vec3(-3.8f,  -2.0f, -12.3f),
+		glm::vec3( 2.4f,  -0.4f,  -3.5f),
+		glm::vec3(-1.7f,   3.0f,  -7.5f),
+		glm::vec3( 1.3f,  -2.0f,  -2.5f),
+		glm::vec3( 1.5f,   2.0f,  -2.5f),
+		glm::vec3( 1.5f,   0.2f,  -1.5f),
+		glm::vec3(-1.3f,   1.0f,  -1.5f)
+	};
+
+	unsigned int VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	/* Position attrib */
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	/* Color attrib */
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	/* Texture position attrib */
+
+	/********************************** Texture ***********************************/
+
+	/* First texture */
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("textures/container.jpg", &width, &height, &nrChannels, 0);
+
+	stbi_set_flip_vertically_on_load(true);
+
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+		std::cout << "Failed to load the texture" << std::endl;
+	stbi_image_free(data);
+
+	/* Second texture */
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	data = stbi_load("textures/awesomeface.png", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+		std::cout << "Failed to load the texture" << std::endl;
+	stbi_image_free(data);
+
+	shader.use();
+	shader.setInt("texture1", 0);
+	shader.setInt("texture2", 1);
+
+	/******************************** 3D matrixes ***********************************/
+	/* View matrix (moves the scene).
+	Moves the scene forward, so the object is further away, the Z-axis is in the reverse direction */
+	glm::mat4 view = glm::mat4(1.0f);
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+	/* Projection matrix (sets the projection style, look)
+	Makes the perspective like real (45 deg), uses screen sizes dividing them to set the size,
+	the near plane is at 0.1 location, the further plane is at 100.0 location */
+	glm::mat4 projection = glm::mat4(1.0f);
+	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+	/* Gets the location of the uniforms */
+	int modelLoc = glGetUniformLocation(shader.ID, "model");
+	int viewLoc = glGetUniformLocation(shader.ID, "view");
+	int projectionLoc = glGetUniformLocation(shader.ID, "projection");
+
+	/* Enables depth test - OpenGL function, that helps with drawing things in front of the others */
+	glEnable(GL_DEPTH_TEST);
+
+	/******************************** Program loop ***********************************/
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
+		/* Clear the color and buffer bit, to not stack them from the previous draw */
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+
+		shader.use();
+
+		glBindVertexArray(VAO);
+		/* Repeat 10 times */
+		for (unsigned int i = 0; i < 10; i++) {
+			/* Translate the model matrix to the given world positions */
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			/* Rotate the 1.0 X, 0.3 Y, 0.5 Z axes by 20 * $i degrees */
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			shader.setMat4("model", model);
+
+			/* Draw the given cube */
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &VAO);
+	glfwTerminate();
+}
+
+
+void fixFrameBufferResize(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
